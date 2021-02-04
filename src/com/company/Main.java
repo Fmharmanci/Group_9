8c999acfba2ac9ceffdefd6dd312edaf6b9e49d3
Fmharmanci <71557439+Fmharmanci@users.noreply.github.com>
2021-02-04 13:33:57 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("welcome all group-9 members");
+         System.out.println("example in git up web page");
     }
 }
