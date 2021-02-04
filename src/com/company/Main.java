d9d5c81a91bd0ef70b00f9d89270cdfc98c0bcaf
Fmharmanci <71557439+Fmharmanci@users.noreply.github.com>
2021-02-04 11:30:51 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("welcome all group-9 members");
         System.out.println("Thanks a lot");
+<<<<<<< Updated upstream
+=======
+        System.out.println("\"example\" = " + "example");
+=======
+        System.out.println("welcome all members");
+>>>>>>> 875b61c (This beginning of project Group-9)
+>>>>>>> Stashed changes
     }
 }
