d202810397bec233822ad2126e1b51156f60a2a0
Fmharmanci <71557439+Fmharmanci@users.noreply.github.com>
2021-02-04 11:25:08 +0300

    last example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("welcome all group-9 members");
         System.out.println("Thanks a lot");
-
         System.out.println("\"example\" = " + "example");
-
-        System.out.println("welcome all members");
-
+        System.out.println("\"example again\" = " + "example again");
     }
 }
